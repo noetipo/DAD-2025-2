1dd20df63bedf7f869e7905861341e257097dfb1
noetipo <noe.tipo@gmail.com>
2025-08-18 16:13:55 -0500

    config server

--- ms-config-server/src/main/java/com/example/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/src/main/java/com/example/msconfigserver/MsConfigServerApplication.java
@@ -3,9 +3,9 @@ package com.example.msconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
+
 public class MsConfigServerApplication {
 
     public static void main(String[] args) {
